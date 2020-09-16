061a4c3c0e467d7185313b9779922979825d09ce
Vinod Purushothaman <purushothaman.vinod@gmail.com>
2020-09-16 12:00:22 +0530

    Updated manin.js and main.ts

--- main.ts
+++ main.ts
@@ -8,3 +8,26 @@ const title = "Kernelon"
 let isBeginner: boolean = true;
 let total: number = 0;
 let name: string = "Vinod";
+let sentance: string = `My name is ${name} 
+I am a beginner in TypeScript`;
+
+console.log(sentance);
+
+let list1: number[] = [1,2,3,4,5];
+let list2: Array<number> = [1,2,3,4,5];
+
+console.log(list1, list2);
+
+let person1: [string, number] = ['Chris', 22];
+
+let person2: [number, string] = [32, 'Leo']
+
+console.log (person1, person2);
+
+enum color {Red, Blue, Green};
+let r: color = color.Red;
+let b: color = color.Blue;
+let g: color = color.Green;
+
+console.log (g, b, r);
+
